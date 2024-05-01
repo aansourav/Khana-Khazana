bc219ed4b0bb0f31c7ae8cb3c07ab02cc7b6a466
SoUrAv <aansourav@gmail.com>
2024-05-01 20:16:58 +0600

    Login Register Page Added

---
+++ app/login/page.js
@@ -0,0 +1,13 @@
+import Login from "../components/Login";
+
+const LoginPage = () => {
+    return (
+        <main className="">
+            <section className="h-screen grid place-items-center">
+                <Login />
+            </section>
+        </main>
+    );
+};
+
+export default LoginPage;
---
+++ app/register/page.js
@@ -0,0 +1,13 @@
+import Register from "../components/Register";
+
+const RegisterPage = () => {
+    return (
+        <main className="">
+            <section className="h-screen grid place-items-center">
+                <Register />
+            </section>
+        </main>
+    );
+};
+
+export default RegisterPage;
